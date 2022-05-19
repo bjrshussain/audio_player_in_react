ea1bdd3fb1db6037fb790cda759b608cc7f0a848
bjrshussain@outlook.com <bjrshussain@outlook.com>
2022-05-19 02:22:49 -0500

    completed simple audio player

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import Player from './Player/Player';
+import { songsdata } from './Player/audios';
+import { useRef, useState, useEffect } from 'react';
+
+const App = () => {
+  const [songs, setSongs] = useState(songsdata);
+  const [isplaying, setisplaying] = useState(false);
+  const [currentSong, setCurrentSong] = useState(songsdata[1]);
+
+  const audioElem = useRef();
+
+  useEffect(() => {
+    if (isplaying) {
+      audioElem.current.play();
+    }
+    else {
+      audioElem.current.pause();
+    }
+  }, [isplaying])
+
+  const onPlaying = () => {
+    const duration = audioElem.current.duration;
+    const ct = audioElem.current.currentTime;
+
+    setCurrentSong({ ...currentSong, "progress": ct / duration * 100, "length": duration })
+
+  }
+
+
+  return (
+    <div className="App">
+      <audio src={currentSong.url} ref={audioElem} onTimeUpdate={onPlaying} />
+      <Player songs={songs} setSongs={setSongs} isplaying={isplaying} setisplaying={setisplaying} audioElem={audioElem} currentSong={currentSong} setCurrentSong={setCurrentSong} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Player/audios.js
@@ -0,0 +1,14 @@
+exports.songsdata = [
+    {
+        "title": "Drake - Forever",
+        "url": "https://beardbarnmusicbucket.s3.amazonaws.com/The+Wild+Horse"
+    },
+    {
+        "title": "Linking Park - In the end",
+        "url": "https://beardbarnmusicbucket.s3.amazonaws.com/The+Wild+Horse"
+    },
+    {
+        "title": "Travis Scott - Stop trina be God",
+        "url": "https://beardbarnmusicbucket.s3.amazonaws.com/The+Wild+Horse"
+    }
+]
